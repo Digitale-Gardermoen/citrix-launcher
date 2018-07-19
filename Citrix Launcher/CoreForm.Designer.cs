8351caf8e2214a806a76e7eef2d3330249d9d6da
Gjermund Madsen <gjemad@gmail.com>
2018-07-19 09:42:58 +0200

    Updated CoreForm design, no need for an actual form.

--- Citrix Launcher/CoreForm.Designer.cs
+++ Citrix Launcher/CoreForm.Designer.cs
@@ -28,7 +28,6 @@ namespace citrix_launcher
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CoreForm));
             this.SuspendLayout();
             // 
             // CoreForm
@@ -36,11 +35,10 @@ namespace citrix_launcher
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(120, 0);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "CoreForm";
-            this.Text = "Starter DV";
             this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
             this.Load += new System.EventHandler(this.CoreForm_Load);
             this.ResumeLayout(false);
